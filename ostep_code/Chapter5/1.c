0dd236141bfd21ad846c7c99939429a9bdd69a03
GRedComeT <a18985113153@gmail.com>
2024-01-17 13:09:23 +0000

    finish code 1

---
+++ ostep_code/Chapter5/1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <assert.h>
+#include <unistd.h>
+
+
+// int main(int argc, char* argv[]) {
+//     int x = 100;
+//     int rc = fork();
+//     assert(rc >= 0);
+//     if (rc == 0) {
+//         printf("I'm child, pid=%d, and the value of x is %d.\n", (int)getpid(), x);
+//     } else {
+//         int rc_wait = wait(NULL);
+//         printf("I'm parent, pid=%d(rc_wait=%d), and child pid=%d, the value of x is %d.\n", (int)getpid(), rc, rc_wait, x);
+//     }
+//     return 0;
+// }
+
+
+
+int main(int argc, char* argv[]) {
+    int x = 100;
+    int rc = fork();
+    assert(rc >= 0);
+    if (rc == 0) {
+        printf("I'm child, pid=%d, and the value of x is %d.\n", (int)getpid(), x);
+        x = 105;
+        printf("I'm child, pid=%d, and the value of x is %d(changed).\n", (int)getpid(), x);
+    } else {
+        int rc_wait = wait(NULL);
+        printf("I'm parent, pid=%d(rc_wait=%d), and child pid=%d, the value of x is %d(child changed).\n", (int)getpid(), rc, rc_wait, x);
+    }
+    return 0;
+}
+
+// easy and simple, because the child and parent is different process, i.e., private register, stack, and heap of process data.
